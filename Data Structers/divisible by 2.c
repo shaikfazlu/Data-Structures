15edf0bd908522b03df1b452ff64d61fbb2b585f
shaikfazlu <121933439+shaikfazlu@users.noreply.github.com>
2023-08-02 15:57:49 +0530

    Add files via upload

---
+++ Data Structers/divisible by 2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("enter a number a:");
+scanf("%d",&a);
+b=a%2;
+if (b==0)
+printf("the number is even\n");
+else
+    printf("the number is odd \n");
+return 0;
+}
---
+++ Data Structers/voting.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("enter your age:");
+scanf("%d",&n);
+if (n>=18)
+{
+printf("you are eligible for voting");}
+else{
+printf("you are not eligible");}
+return 0;
+}
